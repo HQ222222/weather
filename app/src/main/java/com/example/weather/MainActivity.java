dd63fe5335fb5b45ea9e7102b7eb34a676b06dab
HQ222222 <74043003+HQ222222@users.noreply.github.com>
2020-11-11 22:16:17 +0800

    使用LitePal创建和升级数据库

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import org.litepal.LitePal;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("msg","我是一条信息");
+        Log.d("msg", "我是一条信息");
+        LitePal.initialize(this);
+        Button createDatabase = findViewById(R.id.create_database);
+        createDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Book book=new Book();
+                book.setName("hq");
+                book.setAuthor("hqhq");
+                book.setPages(454);
+                book.setPrice(16.94);
+                book.setPress("Unknow");
+                book.save();
+            }
+        });
     }
 }
