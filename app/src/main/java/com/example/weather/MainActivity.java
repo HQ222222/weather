a3e433e20955efcd680418136468c785055323ac
HQ222222 <74043003+HQ222222@users.noreply.github.com>
2020-11-20 15:14:34 +0800

    添加省市区

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -2,14 +2,12 @@ package com.example.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.ColorSpace;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import org.litepal.LitePal;
-import org.litepal.crud.LitePalSupport;
 import org.litepal.tablemanager.Connector;
 
 import java.util.List;
